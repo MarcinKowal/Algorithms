f4bd301f8e4c884ffe99c05fb9db816957d65c10
Marcin Kowal <mmkowal@gmail.com>
2017-04-24 20:34:22 +0200

    Algorithm to remove duplicates from linked list

---
+++ Algorithms/Algorithms.UnitTests/Algorithms/RemoveDuplicatesFromLinkedListTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DataStructures;
+using Algorithms.Algorithms;
+using System.Collections.Generic;
+
+namespace Algorithms.UnitTests.Algorithms
+{
+    [TestClass]
+    public class RemoveDuplicatesFromLinkedListTests
+    {
+        private RemoveDuplicatesFromLinkedList cut;
+
+        [TestInitialize]
+        public void Init()
+        {
+           cut = new RemoveDuplicatesFromLinkedList();
+        }
+        [TestMethod]
+        public void ShouldReturnLinkedListWithoutDup()
+        {
+            var secondList = new SinglyLinkedNode<int>
+            {
+                Data = 1,
+                Next = new SinglyLinkedNode<int>
+                {
+                    Data = 2,
+                    Next = new SinglyLinkedNode<int>
+                    {
+                        Data = 2,
+                        Next = new SinglyLinkedNode<int>
+                        {
+                            Data = 4,
+                            Next = new SinglyLinkedNode<int>
+                            {
+                                Data = 3,
+                                Next = new SinglyLinkedNode<int>
+                                {
+                                    Data = 3,
+                                    Next = new SinglyLinkedNode<int>
+                                    {
+                                        Data = 2,
+                                        Next = null
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var currentNode = cut.RemoveDuplicates(secondList);
+            var expectedList = new List<int> { 1, 2, 4, 3 };
+            var index = 0;
+            while (currentNode.Next != null)
+            {
+                Assert.AreEqual(expectedList[index++], currentNode.Data);
+                currentNode = currentNode.Next;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ShouldThrowExceptionWhenHeadOfListIsNull()
+        {
+            var currentNode = cut.RemoveDuplicates<SinglyLinkedNode<int>>(null);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Algorithms/RemoveDuplicatesFromLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using Algorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Algorithms
+{
+    public class RemoveDuplicatesFromLinkedList
+    {
+        public SinglyLinkedNode<T> RemoveDuplicates<T>(SinglyLinkedNode<T> headOfList)
+        {
+            if (headOfList == null)
+            {
+                throw new ArgumentNullException(nameof(headOfList));
+            }
+
+            var dict = new Dictionary<T, int>();
+
+            var prev = headOfList;
+            var current = headOfList.Next;
+
+            dict.Add(prev.Data, 1);
+            while (current != null)
+            {
+                if (dict.ContainsKey(current.Data))
+                {
+                    prev.Next = current.Next; //removing node 
+                }
+                else
+                {
+                    dict.Add(current.Data, 1);
+                    prev = current;
+                }
+                current = current.Next;
+            }
+            return headOfList;
+        }
+    }
+}
