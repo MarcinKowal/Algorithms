3c7d031631d3cb41078b59f8e3dacd88d5a476f1
Marcin Kowal <mmkowal@gmail.com>
2017-04-24 19:09:34 +0200

    Implement algorithm to merge two sorted linked lists

---
+++ Algorithms/Algorithms.UnitTests/Algorithms/MergeTwoSinglyLinkedListsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.Algorithms;
+using Algorithms.DataStructures;
+using System.Linq;
+
+namespace Algorithms.UnitTests.Algorithms
+{
+    [TestClass]
+    public class MergeTwoSinglyLinkedListsTests
+    {
+        [TestMethod]
+        public void ShouldMergeTwoLists()
+        {
+            var cut = new MergeTwoSinglyLinkedLists();
+            var firstList = new SinglyLinkedNode<int>
+            {
+                Data = 2,
+                Next = new SinglyLinkedNode<int>
+                {
+                    Data = 6,
+                    Next = new SinglyLinkedNode<int>
+                    {
+                        Data = 18,
+                    }
+                }
+            };
+
+            var secondList = new SinglyLinkedNode<int>
+            {
+                Data = 1,
+                Next = new SinglyLinkedNode<int>
+                {
+                    Data = 3,
+                    Next = new SinglyLinkedNode<int>
+                    {
+                        Data = 17,
+                        Next = new SinglyLinkedNode<int>
+                        {
+                            Data = 19,
+                        }
+                    }
+                }
+            };
+            
+            var mergedList = cut.Merge(firstList, secondList);
+            CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 6, 17, 18, 19 }, mergedList.GetAll().ToList());
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Algorithms/MergeTwoSinglyLinkedLists.cs
@@ -0,0 +1,44 @@
+﻿using Algorithms.DataStructures;
+using System;
+
+namespace Algorithms.Algorithms
+{
+    public class MergeTwoSinglyLinkedLists
+    {
+        public SinglyLinkedListImplementation<T> Merge<T>(SinglyLinkedNode<T> headOfFirstList, SinglyLinkedNode<T> headOfSecondList)
+            where T: IComparable<T>
+        {
+            var resultList = new SinglyLinkedListImplementation<T>();
+
+            var firstPointer = headOfFirstList;
+            var secondPointer = headOfSecondList;
+
+            while (firstPointer != null && secondPointer != null)
+            {
+                if (firstPointer.Data.CompareTo(secondPointer.Data) < 0)
+                {
+                    resultList.AddAtEnd(firstPointer.Data);
+                    firstPointer = firstPointer.Next;
+                }
+                else
+                {
+                    resultList.AddAtEnd(secondPointer.Data);
+                    secondPointer = secondPointer.Next;
+                }
+            }
+
+            while(firstPointer != null)
+            {
+                resultList.AddAtEnd(firstPointer.Data);
+                firstPointer = firstPointer.Next;
+            }
+
+            while(secondPointer != null)
+            {
+                resultList.AddAtEnd(secondPointer.Data);
+                secondPointer = secondPointer.Next;
+            }
+            return resultList;
+        }
+    }
+}
